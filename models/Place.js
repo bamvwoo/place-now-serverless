085049ce55bd509b1d2700b5082e732e9c16c27c
bamvwoo <bamvwoo@gmail.com>
2024-08-30 17:08:16 +0900

    mongoose 패키지로 변경

---
+++ models/Place.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+const placeImagesSchema = new mongoose.Schema({
+    url: {
+        type: String,
+        required: true
+    },
+    thumbnail: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const placeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    region: {
+        type: String,
+        required: true
+    },
+    images: {
+        type: [placeImagesSchema],
+        default: []
+    },
+    approved: {
+        type: Boolean,
+        default: false
+    },
+    enabled: {
+        type: Boolean,
+        default: true
+    },
+    admin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const Place = mongoose.models.Place || mongoose.model('Place', placeSchema);
+
+export default Place;
