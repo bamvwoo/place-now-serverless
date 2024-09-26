23437325d248b6f5be6fb74ca31a78d0f6c4eff9
bamvwoo <bamvwoo@gmail.com>
2024-09-26 17:38:25 +0900

    테스트

--- src/components/Common/Button/BasicButton.jsx
+++ src/components/Common/Button/BasicButton.jsx
@@ -1,8 +1,5 @@
 import styled from "styled-components";
 
-let color = "#444";
-let backgroundColor = "#fff";
-
 const BasicButtonBase = styled.button`
     border: 1px solid #444;
     border-radius: 5px;
